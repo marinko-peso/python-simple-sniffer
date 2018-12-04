871ff5da3439ab0d66ef98f26076396add955952
Marinko Peso <marinko.peso@gmail.com>
2018-12-04 13:11:56 +0100

    initial method for ethernet frame :beer:

---
+++ sniffer.py
@@ -0,0 +1,17 @@
+import socket
+import struct
+import textwrap
+
+
+def ethernet_frame(packet):
+    """
+    Unpack ethernet frame.
+    (sync, receiver, sender, type, payload, crc)
+    - dest_mac, src_mac are 6s - 6 characters
+    - H - small unsigned int
+    All this is in the first 14 bytes.
+    socket.htons - format data to readable (little/big indi).
+    Data after first 14 bytes is the actual payload.
+    """
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', packet[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), packet[14:]
